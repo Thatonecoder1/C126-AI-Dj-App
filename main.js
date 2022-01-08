c005ad8b9161b1d87145708121f18c85ef12a7d6
Thatonecoder1 <83841772+Thatonecoder1@users.noreply.github.com>
2022-01-07 18:43:22 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -23,6 +23,29 @@ function draw(){
     image(video,0,0,600,500);
     fill("#FF0000");
     stroke("#FF0000");
+    if(scoreRightWrist > 0.2){
+        circle(rightWristX,rightWristY,20);
+        if(rightWristY >0 && rightWristY <= 100){
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+        }
+        else  if(rightWristY >100 && rightWristY <= 200){
+            document.getElementById("speed").innerHTML = "Speed = 1x";
+            song.rate(1);
+            }
+            else  if(rightWristY >200 && rightWristY <= 300){
+                document.getElementById("speed").innerHTML = "Speed = 1.5x";
+                song.rate(1.5);
+                }
+                else  if(rightWristY >300 && rightWristY <= 400){
+                    document.getElementById("speed").innerHTML = "Speed = 2x";
+                    song.rate(2);
+                    }
+                    else  if(rightWristY >400){
+                        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+                        song.rate(2.5);
+                        }
+    }
 
     if(scoreLeftWrist > 0.2){
         circle(leftWristX,leftWristY,20);
@@ -47,6 +70,10 @@ function modelLoaded(){
 
 function gotPoses(results){
     if(results.length > 0){
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = "+ scoreLeftWrist);
+    
         console.log(results);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -54,5 +81,6 @@ function gotPoses(results){
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX = " + rightWristX + "rightWristY = "+ rightWristY);
+
     }
 }
